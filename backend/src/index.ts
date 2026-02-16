0571f58f332e772020f425da5a431a4ea32daba7
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-16 16:47:07 +0300

    chore: add cors to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = Number(process.env.PORT) || 3000;
-
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.get("/", (request, response) => {
   response.send("Express + TypeScript Server");
 });
