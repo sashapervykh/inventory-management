3e359a8d4ac5957574c06310f9359fbf655c0cf1
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-18 20:36:47 +0300

    feat: change header color

--- frontend/src/shared/ui/AppLayout/AppLayout.tsx
+++ frontend/src/shared/ui/AppLayout/AppLayout.tsx
@@ -2,15 +2,25 @@ import Layout from "antd/es/layout/layout";
 import { Outlet } from "react-router-dom";
 import { AppFooter } from "./Footer/AppFooter";
 import { AppHeader } from "./Header/AppHeader";
+import { ConfigProvider, theme } from "antd";
 
 export function AppLayout() {
   return (
-    <Layout className="flex">
-      <AppHeader />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <AppFooter />
-    </Layout>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+        components: {
+          Layout: { headerBg: "#FFFFFF" },
+        },
+      }}
+    >
+      <Layout className="flex">
+        <AppHeader />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <AppFooter />
+      </Layout>
+    </ConfigProvider>
   );
 }
--- frontend/src/shared/ui/AppLayout/Header/AppHeader.tsx
+++ frontend/src/shared/ui/AppLayout/Header/AppHeader.tsx
@@ -1,9 +1,11 @@
-import { Header } from "antd/es/layout/layout";
 import { SettingsMenu } from "./SettingsMenu/SettingsMenu";
 import { NavMenu } from "./NavMenu/NavMenu";
 import { Logo } from "./Logo/Logo";
 import { AppSearch } from "./AppSearch/AppSearch";
 import { AuthButton } from "./AuthButton/AuthButton";
+import { ConfigProvider, Layout } from "antd";
+
+const { Header } = Layout;
 
 export function AppHeader() {
   return (
