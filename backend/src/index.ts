ed86184e06ab7ebd7829e5c14289c57bc4449965
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-02-20 19:35:40 +0300

    feat: add auth router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import usersRouter from "./routes/users.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const port = Number(process.env.PORT) || 3000;
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 app.get("/", (request, response) => {
   response.status(200).send({ message: "Express + TypeScript Server" });
 });
---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,3 @@
+export class AuthService {}
+
+export const authService = new AuthService();
