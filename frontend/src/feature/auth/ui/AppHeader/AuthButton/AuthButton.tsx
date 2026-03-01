7bc559caa8cc5216512e3c3309b2cb4ffe2c315a
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-01 12:48:18 +0300

    fix: fix ts errors

--- frontend/src/feature/auth/ui/AppHeader/AuthButton/AuthButton.tsx
+++ frontend/src/feature/auth/ui/AppHeader/AuthButton/AuthButton.tsx
@@ -1,11 +1,8 @@
-import Link from "antd/es/typography/Link";
-import { useTranslation } from "react-i18next";
 import { useUser } from "../../../../../entity/user/model/useUser";
 import { LogoutButton } from "./LogoutButton";
 import { LoginLink } from "./LoginLink";
 
 export function AuthButton() {
   const { user } = useUser();
-  const { t } = useTranslation();
   return user ? <LogoutButton /> : <LoginLink />;
 }
