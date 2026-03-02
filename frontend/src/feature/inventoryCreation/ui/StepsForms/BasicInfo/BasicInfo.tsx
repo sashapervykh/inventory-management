7856ca4c60c94686251c27722562f49f4f95e7c5
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-02 12:57:19 +0300

    feat: add tooltips

--- frontend/src/feature/inventoryCreation/ui/StepsForms/BasicInfo/BasicInfo.tsx
+++ frontend/src/feature/inventoryCreation/ui/StepsForms/BasicInfo/BasicInfo.tsx
@@ -1,5 +1,6 @@
 import { Checkbox, Form, Input } from "antd";
-import type { FormDataType } from "../../model/types/FormDataType";
+import type { FormDataType } from "../../../model/types/FormDataType";
+import { InfoCircleOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
@@ -9,21 +10,30 @@ export function BasicInfo() {
       {" "}
       <Form.Item<FormDataType>
         label="Title"
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 20 }}
         name="title"
         rules={[{ required: true, message: "Please input your title!" }]}
+        tooltip="Add the name of your inventory"
       >
         <Input />
       </Form.Item>
       <Form.Item<FormDataType>
         label="Description"
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 20 }}
         name="description"
+        tooltip="Add a description of your inventory. Markdown supported"
         rules={[{ required: true, message: "Please input your description!" }]}
       >
         <TextArea />
       </Form.Item>
       <Form.Item<FormDataType>
+        labelCol={{ span: 3 }}
+        wrapperCol={{ span: 20 }}
         label="Is public?"
         name="isPublic"
+        tooltip="Grant write access to all users"
         valuePropName="checked"
       >
         <Checkbox />
--- frontend/src/feature/inventoryCreation/ui/StepsForms/stepsForms.tsx
+++ frontend/src/feature/inventoryCreation/ui/StepsForms/stepsForms.tsx
@@ -1,4 +1,4 @@
-import { BasicInfo } from "../BasicInfo/BasicInfo";
+import { BasicInfo } from "./BasicInfo/BasicInfo";
 
 export const stepsForms = [
   <BasicInfo />,
