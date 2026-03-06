f03e48fb797f388b69062a5fc03e2b8bc35dcf33
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-06 12:58:11 +0300

    feat: add wrapper for fetch requests

---
+++ frontend/src/shared/api/requestApi.ts
@@ -0,0 +1,20 @@
+import z, { ZodType } from "zod";
+import { API_URL } from "../constants/APU_URL";
+
+export async function requestApi<T extends ZodType>(
+  endpoint: string,
+  schema: T,
+  options?: RequestInit,
+): Promise<z.infer<T>> {
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+  if (!response.ok) {
+    throw new Error(await response.text());
+  }
+
+  const data = await response.json();
+  return schema.parse(data);
+}
