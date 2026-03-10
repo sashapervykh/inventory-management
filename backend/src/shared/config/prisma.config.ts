82a454c8931cc7a42edfd171fd64ae8b07595619
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-10 14:14:59 +0300

    fix: update datasource url

--- backend/src/shared/config/prisma.config.ts
+++ backend/src/shared/config/prisma.config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import { ENV } from "../constants/env.js";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,6 +8,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: ENV.DATABASE_URL,
   },
 });
