e738c7247e100182a36f174d6458945818087ef9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-11 21:15:12 +0300

    fix: remove dependecies from the project root

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -2,7 +2,6 @@ import { useUser } from "../../entity/user/model/useUser";
 
 export function HomePage() {
   const { user } = useUser();
-  console.log(user);
   return user ? (
     <div>
       user {user.firstName} {user.lastName} {user.email}
