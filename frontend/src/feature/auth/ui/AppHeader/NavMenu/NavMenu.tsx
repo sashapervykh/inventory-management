d2381910bd33249620cf547f2ea662c139bae775
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-16 13:32:02 +0300

    fix: change na links color when changing theme

--- frontend/src/feature/auth/ui/AppHeader/NavMenu/NavMenu.tsx
+++ frontend/src/feature/auth/ui/AppHeader/NavMenu/NavMenu.tsx
@@ -2,16 +2,17 @@ import { Menu } from "antd";
 import { getNavItems } from "./navItems";
 import { useLocation } from "react-router-dom";
 import { useUser } from "../../../../../entity/user/model/useUser";
+import { useTheme } from "../../../../../shared/hooks/useTheme/useTheme";
 
 export function NavMenu() {
   const { user } = useUser();
+
   const userType = user && user.type ? user.type : "none";
   const location = useLocation();
   const selectedItem = location.pathname.slice(1);
   return (
     <Menu
       className="w-auto justify-end bg-none"
-      theme="light"
       mode="horizontal"
       defaultSelectedKeys={[selectedItem]}
       items={getNavItems(userType)}
