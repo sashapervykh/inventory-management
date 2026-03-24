e8c1f1319622eac2950e88a22829ede87704c63c
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-03-24 19:28:25 +0300

    feat: add basic account and contact creation flow

--- backend/src/modules/salesforce/salesforce.service.ts
+++ backend/src/modules/salesforce/salesforce.service.ts
@@ -2,8 +2,10 @@ import { ENV } from "../../shared/constants/env.js";
 
 export class SalesforceService {
   createSalesforceEntity = async () => {
-    const accessToken = this.getAccessToken();
-    return accessToken;
+    const accessToken = await this.getAccessToken();
+    const { id } = await this.createAccount(accessToken as string);
+    const { id: contactId } = await this.createContact(accessToken, id);
+    return contactId;
   };
 
   getAccessToken = async () => {
@@ -25,7 +27,60 @@ export class SalesforceService {
     const data = await response.json();
     console.log(data);
 
-    return data;
+    return data.access_token;
+  };
+
+  createAccount = async (accessToken: string, companyName = "Test") => {
+    const response = await fetch(
+      `${ENV.SF_LOGIN_URL}/services/data/v59.0/sobjects/Account`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Name: companyName,
+        }),
+      },
+    );
+
+    const data = await response.json();
+
+    if (Array.isArray(data) && data[0]?.errorCode) {
+      throw new Error(`SF error: ${data[0].errorCode} — ${data[0].message}`);
+    }
+
+    return data as { id: string; success: boolean };
+  };
+
+  createContact = async (accessToken: string, accountId: string) => {
+    const response = await fetch(
+      `${ENV.SF_LOGIN_URL}/services/data/v59.0/sobjects/Contact`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          FirstName: "John",
+          LastName: "Snow",
+          Email: "Snow@email.com",
+          Phone: "+77777777",
+          Title: "Developer",
+          AccountId: accountId,
+        }),
+      },
+    );
+
+    const data = await response.json();
+
+    if (Array.isArray(data) && data[0]?.errorCode) {
+      throw new Error(`SF error: ${data[0].errorCode} — ${data[0].message}`);
+    }
+
+    return data as { id: string; success: boolean };
   };
 }
 
